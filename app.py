4a68656912c0c4f7922c1ef2ae193b21abe271ea
B koushik aryan <125914237+KoushikAryan@users.noreply.github.com>
2023-02-28 16:37:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.title('Portfolio!:wave:')
+
+st.header(':BLUE[_B.KOUSHIK ARYAN_]')
+st.subheader('_ Data scientist_')
+
+st.text('''
+A resilient engineer with expertise in statistical analysis and machine learning.\n
+Proficient in Python, R, SQL, and Tableau.\n
+Skilled in data cleaning, feature engineering, and model evaluation.\n
+Passionate about solving complex problems and eager to contribute to world.\n
+''')
+
+st.header('I invite you to connect me on :')
+st.write(':pushpin: [Likedin](https://www.linkedin.com/in/koushik-aryan-738171177)')
+
